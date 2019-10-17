772fc10c480555a5d81f3847bcff710234caeca9
Denis <denis@efojs.com>
2019-10-18 00:49:21 +0300

    Update names for better readability and add melodies

--- melody.h
+++ melody.h
@@ -1,12 +1,52 @@
 #include "notes_frequencies.h"
 
-// basic scale
-// const float notes[] = { N_C4, N_D4, N_E4, N_F4, N_G4, N_A4, N_B4, N_C5 };
+// Uncomment only one notes[] constant
 
 
-// melodies
+// BASIC SCALE
+//
+// const float notes[] = { C4_N, D4_N, E4_N, F4_N, G4_N, A4_N, B4_N, C5_N };
+
+
+
+// MELODIES
+//
 // Imperial March
+// thanks to https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0133739
+//const float notes[] = {
+//  A3_N, A3_N, A3_N, F3_N, C4_N, A3_N, F3_N, C4_N, A3_N,
+//  E4_N, E4_N, E4_N, F4_N, C4_N, GS3_N, F3_N, C4_N, A3_N//, A4_N, A3_N, A3_N, A4_N, GS4_N, G4_N
+//};
+
+// Yesterday
+// thanks to https://easy-letter-notes.com/yesterday-piano-tabs/
+//const float notes[] = {
+//  D4_N, C4_N, C4_N, E4_N, F4_N, G4_N, A4_N, B4_N, C5_N, B4_N, A4_N, A4_N
+//};
+
+// Jingle Bells
+// thanks to https://www.music-scores.com/midi.php?sheetmusic=Xmas_Jingle_Bells_very_easy_piano
+//const float notes[] = {
+//  E4_N, E4_N, E4_N, E4_N, E4_N, E4_N, E4_N, G4_N, C4_N, D4_N, E4_N,
+////  F4_N, F4_N, F4_N, F4_N, F4_N, E4_N, E4_N, E4_N, E4_N, E4_N, D4_N, D4_N, E4_N, D4_N, G4_N,  
+//};
+
+// Happy Birthday
+// thanks to https://www.letsplaykidsmusic.com/happy-birthday-easy-piano-music/
+//const float notes[] = {
+//  G4_N, G4_N, A4_N, G4_N, C5_N, B4_N, G4_N, G4_N, A4_N, D5_N, C5_N, 
+//  G4_N, G4_N, G5_N, E5_N, C5_N, B4_N, A4_N, F4_N, F4_N, E4_N, C4_N, D4_N, C4_N
+//};
+
+// Smoke On The Water
+// thanks to https://pianoletternotes.blogspot.com/2019/02/smoke-on-water-by-deep-purple.html
+//const float notes[] = {
+//  G4_N, AS4_N, C5_N, G4_N, AS4_N, CS5_N, C5_N, G4_N, AS4_N, C5_N, AS4_N, G4_N
+//};
+
+// Super Mario Bros. Theme
+// thanks to https://noobnotes.net/super-mario-bros-theme-nintendo/
 const float notes[] = {
-  N_A3, N_A3, N_A3, N_F3, N_C4, N_A3, N_F3, N_C4, N_A3,
-  N_E4, N_E4, N_E4, N_F4, N_C4, N_GS3, N_F3, N_C4, N_A3//, N_A4, N_A3, N_A3, N_A4, N_GS4, N_G4
+  C5_N, G4_N, E4_N, A4_N, B4_N, AS4_N, A4_N, G4_N, E5_N, G5_N, A5_N,
+  F5_N, G5_N, E5_N, C5_N, D5_N, B4_N
 };
